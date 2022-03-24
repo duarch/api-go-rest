c52a94421a7c739ca482018f2a04491a399fe717
Andre Duarte <andre.bh@hotmail.com>
2022-03-24 17:23:22 -0300

    Until Middleware

--- routes/routes.go
+++ routes/routes.go
@@ -5,15 +5,18 @@ import (
 	"net/http"
 
 	c "github.com/duarch/go-rest-api/controllers"
+	"github.com/duarch/go-rest-api/middleware"
 	"github.com/gorilla/mux"
 )
 
 func HandleRequests() {
 	r := mux.NewRouter()
+	r.Use(middleware.ContentTypeMiddleware)
 	r.HandleFunc("/", c.Home)
 	r.HandleFunc("/api/personalidades", c.TodasPersonalidades).Methods("GET")
 	r.HandleFunc("/api/personalidades/{id}", c.RetornaUmaPersonalidade).Methods("GET")
 	r.HandleFunc("/api/personalidades", c.CriaUmaNovaPersonalidade).Methods("POST")
 	r.HandleFunc("/api/personalidades/{id}", c.DeletaUmaPersonalidade).Methods("DELETE")
+	r.HandleFunc("/api/personalidades/{id}", c.EditaPersonalidade).Methods("PUT")
 	log.Fatal(http.ListenAndServe(":8000", r))
 }
